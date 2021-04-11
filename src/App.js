bcc336d91e6a05da19c52ef046c94100c2b324ca
martin lowe <martinlowe673@gmail.com>
2021-04-11 11:06:02 +0100

    added todo button

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import "./App.css";
 import Todo from "./Todo";
 
 function App() {
-  const todos = ["Clean Bathroom", "Learn React"];
+  const todos = ["Clean Bathroom", "Learn React", "Code"];
   return (
     <div>
       <h1>Martin To Do</h1>
+      <div>
+        <input></input>
+        <button>Add Todo</button>
+      </div>
       {todos.map((todo) => (
         <Todo todo={todo} />
       ))}
