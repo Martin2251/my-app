e7f76e3100710985f49432df1d43a6d89c279e91
martin lowe <martinlowe673@gmail.com>
2021-04-11 10:26:17 +0100

    rendered prop

--- src/App.js
+++ src/App.js
@@ -2,22 +2,13 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  const todos = ["Clean Bathroom", "Learn React"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Martin To Do</h1>
+      {todos.map((todo) => (
+        <p>{todo}</p>
+      ))}
     </div>
   );
 }
